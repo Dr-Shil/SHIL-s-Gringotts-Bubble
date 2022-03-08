79c6ff5053b714dbdbace55a9156a497d793637f
Sharmili Pandian <101118800+Dr-Shil@users.noreply.github.com>
2022-03-08 19:25:48 +0530

    Add files via upload

---
+++ T2_080322.c
@@ -0,0 +1,12 @@
+/*Trial 2 - Addition*/
+/*Author - Shil*/
+int main()
+{
+    int Number;
+    float Total;
+
+Number = 500;
+Total = 250259.5+200592.25;
+printf("%d\n",Number);
+printf("%7.3f",Total);
+}
